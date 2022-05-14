21b91f53c2d6050f91310aa915dfe8edd1715d4d
kozabrada123 <59031733+kozabrada123@users.noreply.github.com>
2022-05-14 19:03:00 +0200

    Add files via upload

--- presence.py
+++ presence.py
@@ -2,27 +2,59 @@ import threading
 import time
 import settings
 
+import discordsdk as dsdk
+
 import pypresence
 
 class presenceManager:
     def __init__(self):
         self.RPC = None
+        self.app = None
+        self.RPC_Manager = None
         self.sun = ""
         self.moon = ""
 
 
+
     def startPresence(self):
-        self.RPC = pypresence.Presence(client_id=settings.appid)
-        self.RPC.connect()
+        self.app = dsdk.Discord(settings.appid, dsdk.CreateFlags.default)
+        self.RPC_Manager = self.app.get_activity_manager()
+
+
+        self.RPC = dsdk.Activity()
+        self.RPC.details = f"{self.sun}"
+        self.RPC.state = f"{self.moon}"
+        self.RPC.assets.small_image = "https://i.ibb.co/f4xwkTm/Lunaro-logo-pog.png"
+
+        self.RPC_Manager.update_activity(self.RPC, lambda result: self.debugCallback("update_activity", result))
 
     def updatePresence(self, sun, moon):
         self.sun = sun
         self.moon = moon
 
         try:
-            self.RPC.update(small_image="https://i.ibb.co/f4xwkTm/Lunaro-logo-pog.png", details=sun, state=moon)
+            #self.RPC.update(small_image="https://i.ibb.co/f4xwkTm/Lunaro-logo-pog.png", details=sun, state=moon)
+            self.RPC.details = self.sun
+            if self.moon != "":
+                self.RPC.state = self.moon
         except:
-            self.RPC.update(small_image="https://i.ibb.co/f4xwkTm/Lunaro-logo-pog.png", details=f"{sun}, {moon}")
+            self.RPC.details = f"{self.sun}"
+            if self.moon != "":
+                self.RPC.state = f"{self.moon}"
+            #self.RPC.update(small_image="https://i.ibb.co/f4xwkTm/Lunaro-logo-pog.png", details=f"{sun}, {moon}")
+
+        self.RPC_Manager.update_activity(self.RPC, self.debugCallback)
+
+
+    def runCallbacks(self):
+        self.app.run_callbacks()
+
+
+    def debugCallback(self, result):
+        if result == dsdk.Result.ok:
+            print("Successfully set the activity!")
+        else:
+            raise Exception(result)
 
     def stop(self):
         self.RPC.close()
